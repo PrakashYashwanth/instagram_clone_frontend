806d7b3d5850b9eafac3dbba06a1bbf5ebddec42
prakash_yash <myashwanth096@gmail.com>
2023-11-07 18:28:55 +0530

    feat: replicate insta login ui with images and form

--- src/components/atoms/InputComponent/InputComponent.jsx
+++ src/components/atoms/InputComponent/InputComponent.jsx
@@ -1,31 +1,64 @@
 import React, { useState } from 'react'
 import './InputComponent.scss'
 
-const InputComponent = () => {
+// eslint-disable-next-line react/prop-types
+const InputComponent = ({ comp }) => {
 	const [value, setValue] = useState('')
-	return (
-		<div className='_ab32'>
-			<div className='x6s0dn4 xnz67gz x19gtwsn x1nk0tez x1xp9za0 x1hm1hlx x1npaq5j x1c83p5e x1enjb0b x199158v x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x178xt8z xm81vs4 xso031l xy80clv x9f619 x5n08af x78zum5 x1q0g3np xvs91rp x1n2onr6 xh8yej3'>
-				<label className={`_aa48${value ? ' _aa49' : ''}`}>
-					<span className='_aa4a'>Phone number, username, or email</span>
-					<input
-						aria-label='Phone number, username, or email'
-						aria-required='true'
-						autoCapitalize='off'
-						autoCorrect='off'
-						maxLength='75'
-						className='_aa4b _add6 _ac4d'
-						dir=''
-						type='text'
-						value={value}
-						name='username'
-						onChange={(e) => setValue(e.target.value)}
-					/>
-				</label>
-				<div className='x6s0dn4 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xdl72j9 x1q0g3np x1c4vz4f x2lah0s xk390pu x5yr21d xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x1sxyh0 x18d9i69 xkhd6sd x1n2onr6 xxymvpz'></div>
+	const [show, setShow] = useState(false)
+
+	if (comp === 'userName') {
+		return (
+			<div className='input_comp'>
+				<div className='input_comp_div'>
+					<label className={`input_lable${value ? ' afterVal' : ''}`}>
+						<span className='input_lable_text'>
+							Phone number, username, or email
+						</span>
+						<input
+							aria-label='Phone number, username, or email'
+							aria-required='true'
+							maxLength='75'
+							className='input_lable_value'
+							dir=''
+							type='text'
+							value={value}
+							name='username'
+							onChange={(e) => setValue(e.target.value)}
+						/>
+					</label>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	} else if (comp === 'password') {
+		return (
+			<div className='input_comp'>
+				<div className='input_comp_div'>
+					<label className={`input_lable${value ? ' afterVal' : ''}`}>
+						<span className='input_lable_text'>Password</span>
+						<input
+							aria-label='Password'
+							aria-required='true'
+							className='input_lable_value'
+							type={`${show ? 'text' : 'password'}`}
+							value={value}
+							onChange={(e) => setValue(e.target.value)}
+							name='password'
+						/>
+					</label>
+					<div className='password_button_comp'>
+						<button
+							className='password_button'
+							type='button'
+							onClick={() => setShow(!show)}
+							disabled={!value}
+						>
+							{!value ? 'Show' : !show ? `Show` : `Hide`}
+						</button>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default InputComponent
--- src/components/molecules/LoginForm/LoginForm.jsx
+++ src/components/molecules/LoginForm/LoginForm.jsx
@@ -35,94 +35,84 @@ const LoginForm = () => {
 				<div className='login_form_main'>
 					<form className='login_form_ele' id='loginForm' method='post'>
 						<div className='form_outer_div'>
-							{/* <div className='form_inner_div'>
-								<div className='form_username'>
-									<label className='form_username_label'>
-										<span className='_aa4a'>
-											Phone number, username, or email
-										</span>
-										<input
-											aria-label='Phone number, username, or email'
-											aria-required='true'
-											autoCapitalize='off'
-											autoCorrect='off'
-											maxLength='75'
-											className='form_username_value'
-											dir=''
-											type='text'
-											// value='9490536802'
-											name='username'
-										/>
-									</label>
-									<div className='x6s0dn4 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xdl72j9 x1q0g3np x1c4vz4f x2lah0s xk390pu x5yr21d xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x1sxyh0 x18d9i69 xkhd6sd x1n2onr6 xxymvpz'></div>
-								</div>
+							<InputComponent comp='userName' />
+							<InputComponent comp='password' />
+							<button className='login_button' type='submit'>
+								Log in
+							</button>
+							<div className='or_section'>
+								<div className='line_section'></div>
+								<div className='main_or'>or</div>
+								<div className='line_section'></div>
 							</div>
-							<div className='form_inner_div'>
-								<div className='form_password'>
-									<label className='form_password_label'>
-										<span className='_aa4a'>Password</span>
-										<input
-											aria-label='Password'
-											aria-required='true'
-											autoCapitalize='off'
-											autoCorrect='off'
-											className='form_password_value'
-											type='password'
-											// value='jd0!ZLrKW4D0Hbf'
-											name='password'
-										/>
-									</label>
-									<div className='x6s0dn4 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xdl72j9 x1q0g3np x1c4vz4f x2lah0s xk390pu x5yr21d xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x1sxyh0 x18d9i69 xkhd6sd x1n2onr6 xxymvpz'>
-										<div className='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1i64zmx x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1'>
-											<button className='_acan _acao _acat _aj1-' type='button'>
-												Show
-											</button>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1'>
-								<button className='_acan _acap _acas _aj1-' type='submit'>
-									<div className='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1'>
-										Log in
-									</div>
+							<div className='login_fb_section'>
+								<button className='fb_login_button' type='button'>
+									<span className='fb_logo'></span>
+									<span className='fb_button_text'>Log in with Facebook</span>
 								</button>
 							</div>
-							<div className='_ab39'>
-								<div className='x78zum5 x1q0g3np'>
-									<div className='x1iyjqo2 xs83m0k xjm9jq1 x1n2onr6 xwtuau4 x11mr3az'></div>
-									<div className='x1qjc9v5 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1roi4f4 x78zum5 xdt5ytf x1c4vz4f x2lah0s x1nxh6w3 x1s688f x1ly1vsg xdj266r xpdqn1h xat24cr x1sliqq xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xtvhhri x11njtxf'>
-										or
-									</div>
-									<div className='x1iyjqo2 xs83m0k xjm9jq1 x1n2onr6 xwtuau4 x11mr3az'></div>
-								</div>
-							</div>
-							<div className='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1'>
-								<button className='_acan _acao _acas _aj1-' type='button'>
-									<span className=' _a04s _ab36'></span>
-									<span className='_ab37'>Log in with Facebook</span>
-								</button>
-							</div> */}
-							<InputComponent />
 						</div>
 						<a
-							className='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _ab2y _a6hd'
+							className='forgot_password_link'
 							href='/accounts/password/reset/'
 							role='link'
-							tabIndex='0'
 						>
-							<span
-								className='x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye x1fhwpqd xo1l8bm x7l2uk3 x10wh9bi x1wdrske x8viiok x18hxmgj'
-								dir='auto'
-								// style='line-height: var(--base-line-clamp-line-height); --base-line-clamp-line-height: 16px;'
-							>
-								Forgot password?
-							</span>
+							<span className='forgot_password'>Forgot password?</span>
 						</a>
 					</form>
 				</div>
 			</div>
-			<div className='login_form_comp'></div>
+			<div className='login_form_comp'>
+				<span className='no_account'>
+					<p className='signup_para'>
+						Don&apos;t have an account?&nbsp;
+						<a
+							className='singup_link'
+							href='/accounts/emailsignup/'
+							role='link'
+						>
+							<span className='signup_text'>Sign up</span>
+						</a>
+					</p>
+				</span>
+			</div>
+			<div className='get_app_section'>
+				<div className='get_app_text'>
+					<span className='x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp xo1l8bm x5n08af x2b8uid x1tu3fi x3x7a5m x10wh9bi x1wdrske x8viiok x18hxmgj'>
+						Get the app.
+					</span>
+				</div>
+				<div className='get_app_logos'>
+					<a
+						aria-label='Get it on Google Play'
+						className='app_logo'
+						href='https://play.google.com/store/apps/details?id=com.instagram.android&amp;referrer=ig_mid%3D807B13EB-4D1E-4F03-A187-081FE33285A3%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge'
+						rel='nofollow noreferrer'
+						role='link'
+						target='_blank'
+					>
+						<img
+							alt='Get it on Google Play'
+							className='logo_image'
+							src='https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png'
+						/>
+					</a>
+					<a
+						aria-label='Get it from Microsoft'
+						className='app_logo'
+						href='ms-windows-store://pdp/?productid=9nblggh5l9xt&amp;referrer=appbadge&amp;source=www.instagram.com&amp;mode=mini&amp;pos=0%2C0%2C1920%2C1020'
+						rel='nofollow noreferrer'
+						role='link'
+						target='_blank'
+					>
+						<img
+							alt='Get it from Microsoft'
+							className='logo_image'
+							src='https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png'
+						/>
+					</a>
+				</div>
+			</div>
 		</div>
 	)
 }
